454d8a6fddfd9dd51575aa0af2ef6a1acd2874c3
libertadpozos <libertad.pozos@gmail.com>
2019-05-13 12:19:25 +0200

    Add docs

---
+++ docs/precache-manifest.ef1c5817237554b50b71e7a160ad8d73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2b4980c130e2425c62978f3bd31edc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99e49341a4aefaf8f23e",
+    "url": "./static/css/main.c7b7a933.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "99e49341a4aefaf8f23e",
+    "url": "./static/js/main.2cbe22ed.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
